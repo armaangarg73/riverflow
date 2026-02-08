a5fbf775dbb9ef5401b1099a0859ca8a56a36b5d
armaangarg73 <armaangarg755@gmail.com>
2026-02-08 15:56:24 +0530

    add attributes and indexes

--- src/models/server/question.collection.ts
+++ src/models/server/question.collection.ts
@@ -1,4 +1,4 @@
-import { Permission } from "node-appwrite";
+import { IndexType, Permission, OrderBy } from "node-appwrite";
 import { db, questionCollection } from "../name";
 import { databases } from "./config";
 
@@ -12,4 +12,57 @@ export default async function createQuestionCollection()  {
         Permission.delete("users"),
     ])
     console.log("Question collection is created");
+
+    // creating attributes
+
+    await Promise.all([
+        databases.createStringAttribute(db, questionCollection, "title", 100, true),
+        databases.createStringAttribute(db, questionCollection, "content", 1000, true),
+        databases.createStringAttribute(db, questionCollection, "authorId", 50, true),
+        databases.createStringAttribute(db, questionCollection, "tags", 50, true , undefined , true),
+        databases.createStringAttribute(db, questionCollection, "attachmentId", 50, false),
+    ])
+    console.log("Question Attributes created");
+
+    // creating indexes
+    await Promise.all([
+      databases.createIndex(
+        db,
+        questionCollection,
+        "title",
+        IndexType.Fulltext,
+        ["title"],
+        [OrderBy.Asc],
+      ),
+      databases.createIndex(
+        db,
+        questionCollection,
+        "content",
+        IndexType.Fulltext,
+        ["content"],
+        [OrderBy.Asc],
+      ),
+      databases.createIndex(
+        db,
+        questionCollection,
+        "question_author_index",
+        IndexType.Key,
+        ["authorId"],
+      ),
+      databases.createIndex(
+        db,
+        questionCollection,
+        "question_tags_index",
+        IndexType.Key,
+        ["tags"],
+      ),
+      databases.createIndex(
+        db,
+        questionCollection,
+        "question_attachment_index",
+        IndexType.Key,
+        ["attachmentId"],
+      ),
+    ]);
+     console.log("Question indexes created");
 }
