ff36a8489d76d10f7cfcbee39e0c2cda0b3684a4
armaangarg73 <armaangarg755@gmail.com>
2026-04-13 14:12:42 +0530

    Type safety in comments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -22,7 +22,9 @@ const Comments = ({
   typeId: string;
   className?: string;
 }) => {
-  const [comments, setComments] = React.useState(_comments);
+  
+  const [comments, setComments] = React.useState<any>(_comments);
+
   const [newComment, setNewComment] = React.useState("");
   const { user } = useAuthStore();
 
@@ -43,8 +45,8 @@ const Comments = ({
         },
       );
 
-      setNewComment(() => "");
-      setComments((prev) => ({
+      setNewComment("");
+      setComments((prev: any) => ({
         total: prev.total + 1,
         documents: [{ ...response, author: user }, ...prev.documents],
       }));
@@ -57,10 +59,10 @@ const Comments = ({
     try {
       await databases.deleteDocument(db, commentCollection, commentId);
 
-      setComments((prev) => ({
+      setComments((prev: any) => ({
         total: prev.total - 1,
         documents: prev.documents.filter(
-          (comment) => comment.$id !== commentId,
+          (comment: any) => comment.$id !== commentId,
         ),
       }));
     } catch (error: any) {
@@ -70,42 +72,49 @@ const Comments = ({
 
   return (
     <div className={cn("flex flex-col gap-2 pl-4", className)}>
-      {comments.documents.map((comment) => (
+      {comments.documents.map((comment: any) => (
         <React.Fragment key={comment.$id}>
           <hr className="border-white/40" />
+
           <div className="flex gap-2">
             <p className="text-sm">
               {comment.content} -{" "}
               <Link
-                href={`/users/${comment.authorId}/${slugify(comment.author.name)}`}
+                href={`/users/${comment.authorId}/${slugify(
+                  comment.author?.name || "user",
+                )}`}
                 className="text-orange-500 hover:text-orange-600"
               >
-                {comment.author.name}
+                {comment.author?.name || "User"}
               </Link>{" "}
               <span className="opacity-60">
                 {convertDateToRelativeTime(new Date(comment.$createdAt))}
               </span>
             </p>
-            {user?.$id === comment.authorId ? (
+
+            {user?.$id === comment.authorId && (
               <button
                 onClick={() => deleteComment(comment.$id)}
                 className="shrink-0 text-red-500 hover:text-red-600"
               >
                 <IconTrash className="h-4 w-4" />
               </button>
-            ) : null}
+            )}
           </div>
         </React.Fragment>
       ))}
+
       <hr className="border-white/40" />
+
       <form onSubmit={handleSubmit} className="flex items-center gap-2">
         <textarea
           className="w-full rounded-md border border-white/20 bg-white/10 p-2 outline-none"
           rows={1}
           placeholder="Add a comment..."
           value={newComment}
-          onChange={(e) => setNewComment(() => e.target.value)}
+          onChange={(e) => setNewComment(e.target.value)}
         />
+
         <button className="shrink-0 rounded bg-orange-500 px-4 py-2 font-bold text-white hover:bg-orange-600">
           Add Comment
         </button>
