40f40ba98f49e06dfdb80ff5d88f9c9e03c03f57
armaangarg73 <armaangarg755@gmail.com>
2026-04-13 12:50:29 +0530

    Added cahe and fixes in edit page

--- src/app/questions/[quesId]/[quesName]/edit/page.tsx
+++ src/app/questions/[quesId]/[quesName]/edit/page.tsx
@@ -2,17 +2,23 @@ import { db, questionCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
 import React from "react";
 import EditQues from "./EditQues";
+import { Models } from "appwrite";
+
+type Question = Models.Document & {
+  authorId: string;
+  title: string;
+};
 
 const Page = async ({
   params,
 }: {
   params: { quesId: string; quesName: string };
 }) => {
-  const question = await databases.getDocument(
+  const question = (await databases.getDocument(
     db,
     questionCollection,
     params.quesId,
-  );
+  )) as Question;
 
   return <EditQues question={question} />;
 };
